c2c84b4c0d66c1d26594214e4ff4ffec2828f0a1
Tia Fields <tia.fields226@gmail.com>
2024-01-13 13:56:45 -0500

    adding components folder with navigation.js and installing yarn

---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navigation() {
+    return (<nav className="site-navigation">
+        <span>Tia's Blog</span>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+
+
+    </nav>)
+}
